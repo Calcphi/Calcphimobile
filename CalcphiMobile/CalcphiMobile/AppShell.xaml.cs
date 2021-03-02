4fd37004129f657720de6e439cd6e431db9141f4
Diogo Rocha <diogo.rocha@zonmail.pt>
2021-03-02 17:48:12 +0000

    The UI should now work on 75% of screens and added the about us button

--- CalcphiMobile/CalcphiMobile/AppShell.xaml.cs
+++ CalcphiMobile/CalcphiMobile/AppShell.xaml.cs
@@ -3,6 +3,8 @@ using CalcphiMobile.Views;
 using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+
 
 namespace CalcphiMobile
 {
@@ -18,6 +20,16 @@ namespace CalcphiMobile
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private async void About_us(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://calcphi.netlify.app/");
+
         }
     }
 }

2574b58ade41ed3493aa009ce602aef1319d333f
Diogo Rocha <diogo.rocha@zonmail.pt>
2021-03-05 19:06:05 +0000

    Calculator should now be working with the exception of  roots, pi and fractions.

--- CalcphiMobile/CalcphiMobile/ViewModels/MainScreenViewModel.cs
+++ CalcphiMobile/CalcphiMobile/ViewModels/MainScreenViewModel.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CalcphiMobile.Models;
+using CalcphiMobile.ViewModels;
+using CalcphiMobile.Views;
 using Xamarin.Forms;
 using Xamarin.Essentials;
 using System.Windows.Input;
+using System.ComponentModel;
 
 
 namespace CalcphiMobile.ViewModels
 {
-    public class MainScreenViewModel : BaseViewModel
+    public class MainScreenViewModel
     {
-        public MainScreenViewModel()
-        {
-            Title = "Calcphi";
+        string Equation = "hello";
 
+        void One(object sender, EventArgs e)
+        {
+            Equation = Equation + "1";
         }
+        void Two(object sender, EventArgs e)
+        {
+            Equation = Equation + "2";
+        }
+        void Plus(object sender, EventArgs e)
+        {
+            Equation = Equation + "+";
+        }
+
     }
 }
--- CalcphiMobile/CalcphiMobile/Views/MainScreen.xaml.cs
+++ CalcphiMobile/CalcphiMobile/Views/MainScreen.xaml.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CalcphiMobile.Models;
+using CalcphiMobile.ViewModels;
+using CalcphiMobile.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Athena_Engine;
 
 namespace CalcphiMobile.Views
 {
@@ -16,7 +19,275 @@ namespace CalcphiMobile.Views
         {
 
             InitializeComponent();
-            Title = "Calcphi";
+            Equation.Text = "";
+            
         }
+
+        void Zero(object sender, EventArgs e)
+        {
+
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "0";
+        }
+
+        void One(object sender, EventArgs e)
+        {
+            
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "1";
+        }
+        void Two(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "2";
+        }
+        void Three(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "3";
+        }
+        void Four(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "4";
+        }
+        void Five(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "5";
+        }
+        void Six(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "6";
+        }
+        void Seven(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "7";
+        }
+        void Eight(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "8";
+        }
+        void Nine(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "9";
+        }
+        void Dot(object sender, EventArgs e)
+        {
+
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + ".";
+        }
+
+        void Plus(object sender, EventArgs e)
+        {
+
+            if (Result.Text != "")
+            {
+                Equation.Text = Result.Text; 
+                Result.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "+";
+        }
+        void Minus(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Equation.Text = Result.Text;
+                Result.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "-";
+        }
+        void Multiplication(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Equation.Text = Result.Text;
+                Result.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "*";
+        }
+        void Division(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Equation.Text = Result.Text;
+                Result.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "/";
+        }
+        void Exponent(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Equation.Text = Result.Text;
+                Result.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "^";
+        }
+        void OpenBrackets(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "(";
+        }
+        void CloseBrackets(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + ")";
+        }
+
+
+        void EXP(object sender, EventArgs e)
+        {
+
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = "";
+
+            }
+
+            Equation.Text = Equation.Text + "*10^";
+        }
+        void Backspace(object sender, EventArgs e)
+        {
+            if (Result.Text != "")
+            {
+                Result.Text = "";
+                Equation.Text = Equation.Text.Substring(0, Equation.Text.Length);
+
+            }
+            if (Equation.Text == "")
+            {
+                Equation.Text = "";
+
+            }
+            else
+            {
+                Equation.Text = Equation.Text.Remove(Equation.Text.Length - 1);
+
+            }
+
+            
+        }
+
+
+        Tokenizer t = new Tokenizer();
+        Parser p = new Parser();
+        Solver s = new Solver();
+
+        void Equal(object sender, EventArgs e)
+        {
+
+            if (Equation.Text == "")
+            {
+                Equation.Text = "";
+
+            }
+            else
+            {
+                Equation.Text.Replace(",", ".");
+                List<Node> nodes = t.Tokenize(Equation.Text);
+                Node nf = p.Parse(nodes);
+                Result.Text = Convert.ToString(s.Solve(nf));
+
+
+            }
+
+
+        }
+
     }
 }
